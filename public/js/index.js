e86c833fe047346ea97bd92adbb1834676dac5c6
Philip Scott <pscott@zeptohost.com>
2017-08-10 00:37:05 -0400

    Add invisible recaptcha. Watch your content-types

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,31 @@
 var socket = io('/lobby', {transports: ['websocket']});
 angular.module('landingPage', [])
-  .controller('UrlGeneratorCtrl', ['$scope', function($scope) {
+  .controller('UrlGeneratorCtrl', ['$scope', '$window', '$http', function($scope, $window, $http) {
     //ill make a factory later
     var quoteDom = document.getElementById('quote');
     var authorDom = document.getElementById('author');
     var linkDom = document.getElementById('room-url');
-    $scope.url = '';
-    $scope.generateRoom = function() {
-      socket.emit('request room');
-    };
-    socket.on('room generated', function(data) {
-      //apply is necessary when doing callbacks
-      $scope.$apply(function() {
-        $scope.url = data;
+
+    $scope.login = function(token) {
+      console.log(token);
+      $http({
+        method: 'POST',
+        url: '/recaptcha',
+        data: token,
+        headers: {'Content-Type': 'text/plain'}
+      }).then(function(res) {
+        console.log(res.data);
+        $scope.$applyAsync(function() {
+          $scope.url = res.data;
+        });
+        angular.element(linkDom).css('opacity', 1);
+      }, function(err) {
+        alert(err);
       });
-      angular.element(linkDom).css('opacity', 1);
-    });
+    };
+    $window.login = $scope.login;
+
+    $scope.url = '';
     socket.on('daily quote', function(data) {
       angular.element(quoteDom).html(data.quote);
       angular.element(authorDom).html('&mdash; ' + data.author);
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 let http = require('http');
+let request = require('request');
 let express = require('express');
 let shortid = require('shortid');
 let optional = require('optional');
-let localKeys = optional('./config/mashapeKeys');
+let bodyParser = require('body-parser');
+let mashapeKeys = optional('./config/mashapeKeys');
+let recaptchaKeys = optional('./config/recaptchaKeys');
 
 const WEBSERVER_PORT = process.env.PORT || 8080;
-const QUOTE_KEY = process.env.QUOTE_KEY_PROD || localKeys.PROD;
+const QUOTE_KEY = process.env.QUOTE_KEY_PROD || mashapeKeys.PROD;
+const RECAPTCHA_KEY = process.env.RECAPTCHA_KEY || recaptchaKeys.SECRET;
 
 let app = express();
 let server = http.Server(app);
@@ -19,12 +23,15 @@ let url;
 
 io.set('transports', ['websocket']);
 app.use(express.static('public'));
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.get('/', function sendLobbyPage(req, res) {
   url = req.protocol + '://' + req.get('host');
   res.sendFile(__dirname + '/index.html');
 });
-app.get('/r/:room', function sendRoomPage(req, res) {
+app.get('/:room', function sendRoomPage(req, res) {
   let address = req.params.room;
   if (appManager.roomExists(address)) {
     res.sendFile(__dirname + '/room.html');
@@ -33,14 +40,29 @@ app.get('/r/:room', function sendRoomPage(req, res) {
   }
 });
 
+app.post('/recaptcha', function validateRecaptcha(req, res) {
+  console.log('Post sent to Google');
+  console.log(RECAPTCHA_KEY);
+  console.log(req.body);
+  recaptchaURL = 'https://www.google.com/recaptcha/api/siteverify?secret=' + RECAPTCHA_KEY + '&response=' + req.body + '&remoteip=' + req.connection.remoteAddress;
+  request(recaptchaURL, function handleGoogleReply(err, googleRes, body) {
+    console.log('Post received from Google');
+    if (JSON.parse(body).success) {
+      let address = shortid.generate();
+      res.send(url + '/' + address);
+      appManager.genRoom(address);
+    } else {
+      res.send('There\'s an error with the ReCaptcha, sorry :c');
+    }
+  });
+});
+
+
+
 lobbyNsp.on('connection', (socket) => {
   console.log('somebody connected in the lobby');
   socket.emit('daily quote', quoteMaker.getQuote());
-  socket.on('request room', () => {
-    let address = shortid.generate();
-    socket.emit('room generated', url + '/r/' + address);
-    appManager.genRoom(address);
-  });
+
 });
 
 setInterval( () => {
